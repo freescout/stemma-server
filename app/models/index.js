9c253bc17ec81fbb117ddbf1e4582db83c0ef8fa
freescout <neethu.jacob@gmail.com>
2021-01-11 15:18:11 +0100

    added mongoURI in index.js

--- app/models/index.js
+++ app/models/index.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 
 const db = {};
 db.mongoose = mongoose;
-db.url = dbConfig.url;
+db.url = dbConfig.mongoURI;
 db.individuals = require('./individual.model.js')(mongoose);
 
 module.exports = db;
